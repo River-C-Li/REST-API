3a78b7c463ea6f71acaaa48c6221bb9137f5e9a1
River-C-Li <chengrui8848@gmail.com>
2024-11-16 18:47:26 -0500

    Add files via upload

---
+++ data/comments.js
@@ -0,0 +1,19 @@
+const comments = [
+    {
+      id: 1,
+      userId: 1,
+      postId: "post id 1 here",
+      body:
+        "This is comment one.",
+    },
+    {
+      id: 2,
+      userId: 1,
+      postId: "post id 2 here",
+      body:
+        "This is comment two.",
+    },
+  ];
+  
+  module.exports = comments;
+  
